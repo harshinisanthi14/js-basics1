bbd747aa55e4e9782e388716925cb7e81c0393b1
harshinisanthi14 <harshiniudayagiri123@gmail.com>
2024-10-27 11:30:11 +0530

    js8

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function greet(name) {
-    console.log('hello ' + name);
+function greet(name,age) {
+    console.log('hello ' + name + ' , ' + age);
 }
 
-greet('Harshi');
+greet('Harshi',21);
+greet('bintu',19);
+greet('bhar',20);
