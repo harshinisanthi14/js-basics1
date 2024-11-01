cffdda9c3ebe13df4a8ab91c008616615c7a0d4c
harshinisanthi14 <harshiniudayagiri123@gmail.com>
2024-11-01 14:24:49 +0530

    kej

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const element=document.getElementById("fruits");
+const element=document.getElementById("desserts");
 const children =element.children;
- Array.from(children).forEach(child => {
-    child.style.backgroundColor="yellow";
- });
+
+    children[1].style.backgroundColor="yellow";
+ 
