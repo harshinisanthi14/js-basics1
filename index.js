0b24039739358633c0fbfc10139ae55417d3c6c3
harshinisanthi14 <harshiniudayagiri123@gmail.com>
2024-11-01 14:21:36 +0530

    chi

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const element=document.getElementById("mango");
-const parent =element.parentElement;
-parent.style.backgroundColor="yellow";
+const element=document.getElementById("fruits");
+const children =element.children;
+ Array.from(children).forEach(child => {
+    child.style.backgroundColor="yellow";
+ });
